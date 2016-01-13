c94d8b17af3ecbf9d01ecd196bce1c940391b7ca
larry-fuy <larry.yong.fu@gmail.com>
2016-01-13 15:52:14 -0600

    quiet install Java 8

--- web/fetchtao/pkg_scripts/preInstall.sh
+++ web/fetchtao/pkg_scripts/preInstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:openjdk-r/ppa
+sudo add-apt-repository -y ppa:openjdk-r/ppa
 # deb repo should live
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install -y openjdk-8-jdk
 
 /usr/bin/update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 1
 /usr/bin/update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
