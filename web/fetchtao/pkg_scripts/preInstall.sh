955ed6321a9408a7b1f62bde8d63d9bf7f542e18
larry-fuy <larry.yong.fu@gmail.com>
2016-01-13 15:34:49 -0600

    add jdk 8

--- web/fetchtao/pkg_scripts/preInstall.sh
+++ web/fetchtao/pkg_scripts/preInstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-/usr/bin/update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java 1
-/usr/bin/update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+sudo add-apt-repository ppa:openjdk-r/ppa
+# deb repo should live
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+
+/usr/bin/update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 1
+/usr/bin/update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
 # check for Java installation
 if type java; then
