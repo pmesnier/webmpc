7e11b97f3be810acca2bba62121715ae99b2c4f9
larry-fuy <larry.yong.fu@gmail.com>
2016-01-15 09:00:43 -0600

    Java 7 support

--- web/fetchtao/pkg_scripts/preInstall.sh
+++ web/fetchtao/pkg_scripts/preInstall.sh
@@ -5,8 +5,8 @@
 # sudo apt-get update
 # sudo apt-get install -y openjdk-8-jdk
 
-# /usr/bin/update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 1
-# /usr/bin/update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+/usr/bin/update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java 1
+/usr/bin/update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
 
 # check for Java installation
 if type java; then
